SW-Rugi/keyuri

Initial project skeleton, README.md, and LICENSE.txt
MIT Licensed

ruby/lib/keyuri/version.rb
@@ -1,0 +1,3 @@
+module KeyURI
+  VERSION = "0.0.0".freeze
+end

ruby/spec/spec_helper.rb
@@ -1,0 +1,14 @@
+require "bundler/setup"
+require "keyuri"
+
+RSpec.configure do |config|
+  # Monkeypatching sucks!
+  config.disable_monkey_patching!
+
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end

miscreant/xstream

ruby/lib/xstream/version.rb
@@ -1,0 +1,3 @@
+module XStream
+  VERSION = "0.0.0".freeze
+end

ruby/spec/spec_helper.rb
@@ -1,0 +1,14 @@
+require "bundler/setup"
+require "xstream"
+
+RSpec.configure do |config|
+  # Monkeypatching sucks!
+  config.disable_monkey_patching!
+
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end